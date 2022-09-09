9ce8c5e2ddb3a44e393c4f162643325d5800d901
EamonnLi <it_yiyaun@163.com>
2022-09-09 14:55:55 +0800

    vo

---
+++ Assets/VO/Scripts/VOActor.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VOActor : MonoBehaviour
+{
+    public Vector3 speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Time.deltaTime * speed;
+    }
+}
